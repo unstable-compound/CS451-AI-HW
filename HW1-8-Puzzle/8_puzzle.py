0185193917edfc7050d1c3d63fb341439852b92e
curti <culewis@pdx.edu>
2020-01-31 17:27:41 -0800

    A* implementation is working
    Next: Flesh out test driver and implement greedy best first

--- HW1-8-Puzzle/8_puzzle.py
+++ HW1-8-Puzzle/8_puzzle.py
@@ -1,6 +1,7 @@
 import itertools
 from queue import PriorityQueue
 from sys import argv
+import sys
 
 '''
 Abstract formulation of problem instance.
@@ -62,7 +63,7 @@ class Problem:
     def get_blank_space_index(self, state):
         current = 0
         for i in range(0, 9):
-            if self.current_state[i] == 'b':
+            if state[i] == 'b':
                 current = i
         return current
 
@@ -74,7 +75,9 @@ class Problem:
 
     def actions(self, state):
         blank_index = self.get_blank_space_index(state)
-        return allowable_moves_map[blank_index]
+        allowed_moves = allowable_moves_map[blank_index]
+        return allowed_moves
+
 
     def goal_test(self, state):
         for i in range(0, len(state)):
@@ -91,32 +94,109 @@ def check_parity(s, g):
     # replace 'b' with 9
     start = list(s)
     goal = list(g)
-    for i in range(0, 9):
-        if start[i] == 'b':
-            start[i] = 9
-        if goal[i] == 'b':
-            goal[i] = 9
+
 
     inversions_start = 0
     inversions_goal = 0
     for i in range(0, 9):
         x = start[i]
-        for j in range(i + 1, 9):
-            if x > start[j]:
-                inversions_start += 1
+        if x is not 'b':
+            for j in range(i + 1, 9):
+                if start[j] is 'b':
+                    continue
+                if x > start[j]:
+                    inversions_start += 1
         y = goal[i]
-        for j in range(i + 1, 9):
-            if y > goal[j]:
-                inversions_goal += 1
+        if y is not 'b':
+            for j in range(i + 1, 9):
+                if goal[j] is 'b':
+                    continue
+                if y > goal[j]:
+                    inversions_goal += 1
 
     if inversions_start % 2 == inversions_goal % 2:
         return True
     return False
 
 
+def getCurrent(openSet, fScore):
+    lowest = p_infinity
+    retval = None
+    for state in openSet:
+        f = fScore[state]
+        if f < lowest:
+            lowest = f
+            retval = state
+    return retval
+
+
+def reconstruct_path(cameFrom, current):
+    path_list = list()
+    path_list.append(current)
+    while current in cameFrom.keys():
+        to_add = cameFrom[current]
+        path_list.append(to_add)
+        current = to_add
+    return path_list
+
+
+def new_a_star(problem, hueristic):
+    openSet = set() #frontier
+    closedSet = set() #explored
+    cameFrom = dict() #path
+
+    gScore = dict()
+    fScore = dict()
+
+    start = tuple(problem.initial_state)
+    goal = tuple(problem.goal)
+    openSet.add(start)
+
+    gScore[start] = 0
+    fScore[start] = hueristic(start, goal)
+
+    while len(openSet) != 0:
+        current = getCurrent(openSet, fScore)
+        if problem.goal_test(current):
+            solution = reconstruct_path(cameFrom, current)
+            solution = solution[::-1]
+            return solution
+
+        openSet.remove(current)
+        closedSet.add(current)
+
+        """*****************"""
+        for action in problem.actions(current):
+            child_state = tuple(problem.RESULT(current, action))
+            f_keys = fScore.keys()
+            g_keys = gScore.keys()
+            if child_state not in f_keys:  # gscore and fscore are supposed to be default infinity
+                fScore[child_state] = p_infinity
+            if child_state not in g_keys:
+                gScore[child_state] = p_infinity
+
+            if child_state not in closedSet and child_state not in openSet: # discovered a new state
+                openSet.add(child_state)
+            if child_state in closedSet:
+                continue
+
+            tent_g_score = gScore[current] + 1  # is 1 for all children cause the distance is same for each move
+            if tent_g_score >= gScore[child_state]:
+                continue  # this is not a better path
+
+            ## This path is best so far, so record it
+            cameFrom[child_state] = current
+            gScore[child_state] = tent_g_score
+            fScore[child_state] = tent_g_score + hueristic(child_state, goal)
+
+    # if end while loop, then openset is empty and we have no solution
+    return None
+
+
 def puzzle_solve(initial_state_array, goal_state_array, search_algorithm, hueristic):
     if not check_parity(initial_state_array, goal_state_array):
         print("This puzzle is not solvable")
+        return
     problem = Problem(initial_state_array, goal_state_array)
     solution = search_algorithm(problem, hueristic)
     if solution is None:
@@ -124,15 +204,16 @@ def puzzle_solve(initial_state_array, goal_state_array, search_algorithm, hueris
     print("The solution, beginning with initial state and progressing to")
     print("the final (goal) state in order of moves taken is:")
     for item in solution:  # (state, action)
-        print("\nThe blank space move to the next state is:")
-        print(item[1])
-        print("State: \t")
-        row_one = item[0][0:3]
-        row_two = item[0][3:6]
-        row_three = item[0][6:9]
-        print(row_one)
-        print(row_two)
-        print(row_three)
+        item_state = item
+        sys.stdout.write(str(item_state))
+        sys.stdout.write("-->")
+
+       # row_one = item_state[0:3]
+       # row_two = item_state[3:6]
+       # row_three = item_state[6:9]
+       # print(row_one)
+       # print(row_two)
+       # print(row_three)
 
     # TODO: Finish
 
@@ -159,7 +240,7 @@ def number_misplaced_nodes(state, goal_state):
 
 def make_child_node(problem, parent, action):
     new_state = problem.RESULT(parent.state, action)
-    path_cost = parent.cost + step_cost(parent.state, action)  # TODO implement stepcost
+    path_cost = parent.cost + 1
     return SearchNode(new_state, parent, action, path_cost, parent.depth + 1)
 
 
@@ -195,18 +276,19 @@ def get_current(known_items, fScore):  # known_items
 
 
 def a_star_search(problem, hueristic):
+    map_states_to_nodes = dict()
     # Set of nodes already evaluated
-    visited_items = set()  # closedSet
+    closedSet = set()  # closedSet
+    evaluated_states = set()
     def_map_val = p_infinity
     # set of known nodes which have yet to be evaluated:
-    known_items = set()  # openSet
+    openSet = set()  # openSet
     known_states = set()
-    # known_items is set of tuples:
-
     goal = problem.goal
     # node ←a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
     node = SearchNode(problem.initial_state, None, None, 0, 0)
-    known_items.add(node)
+    map_states_to_nodes[tuple(node.state)] = node
+    openSet.add(node)
     known_states.add(tuple(node.state))
 
     came_from = dict()
@@ -227,37 +309,51 @@ def a_star_search(problem, hueristic):
     # For the first node, (start), that value is completely heuristic
     fScore[tuple(node.state)] = hueristic(node.state, goal_state)
 
-    while len(known_items) != 0:
-        current = get_current(known_items, fScore)
+    while len(openSet) != 0:
+        current = get_current(openSet, fScore)
         if current is None:
             print("Failure to find valid node in known set")
             return None
         if problem.goal_test(current.state):
             return solutions(current)
-        known_items.remove(current)  # openSet
+        openSet.remove(current)  # openSet
         known_states.remove(tuple(current.state))
-        visited_items.add(current)  # closedSet
+        closedSet.add(current)  # closedSet
+        evaluated_states.add(tuple(current.state))
 
         # grab neighbors
         for action in problem.actions(node.state):
-            child = make_child_node(problem, node, action)
-            for item in visited_items:
-                if item.state == child.state:
-                    continue
+            # check if we need new node or have the node already
+            new_state = problem.RESULT(node.state, action)
+            if tuple(new_state) in map_states_to_nodes.keys():
+                child = map_states_to_nodes[tuple(new_state)]
+            else:
+                # generate child node
+                child = make_child_node(problem, node, action)
+                f_keys = fScore.keys()
+                g_keys = gScore.keys()
+                if tuple(child.state) not in f_keys:  # gscore and fscore are supposed to be default infinity
+                    fScore[tuple(child.state)] = p_infinity
+                if tuple(child.state) not in g_keys:
+                    gScore[tuple(child.state)] = p_infinity
+                # add to state node map
+                map_states_to_nodes[tuple(child.state)] = child
+
+            if tuple(child.state) in evaluated_states:
+                continue
+                # this node was already evaluated
             if tuple(child.state) not in known_states:  # new node discovered
-                known_items.add(child)
+                openSet.add(child)
                 known_states.add(tuple(child.state))
                 # set default g and f scores
-                fScore[tuple(child.state)] = p_infinity
-                gScore[tuple(child.state)] = p_infinity
+            tentative_gscore = gScore[tuple(current.state)] + 1
+            if tentative_gscore >= gScore[tuple(child.state)]:
+                continue
 
-            tentative_gscore = gScore[tuple(current.state)] + (child.depth - current.depth)
-            if tentative_gscore < gScore[tuple(child.state)]:
-                  # so far this is best path so record it
-                    came_from[child] = current
-                    gScore[tuple(child.state)] = tentative_gscore
-                    fScore[tuple(child.state)] = gScore[tuple(child.state)] + hueristic(child.state, goal_state)
-
+            # so far this is best path so record it
+            came_from[child] = current
+            gScore[tuple(child.state)] = tentative_gscore
+            fScore[tuple(child.state)] = gScore[tuple(child.state)] + hueristic(child.state, goal_state)
 
     # if end while loop then fail
     return None
@@ -355,36 +451,82 @@ class PuzzleSolver:
             'R': +1
         }
 
-        '''
-        Greedy-best-first-algorithm:  Modify for tree search.
-        
-        
-        function UNIFORM-COST-SEARCH(problem) returns a solution, or failure
-        node ←a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
-        frontier ←a priority queue ordered by f(n), with node as the only element
-        
-        loop do
-        if EMPTY?( frontier) then return failure
-        node←POP( frontier ) /* chooses the lowest-cost node in frontier */
-        if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
-        
-        for each action in problem.ACTIONS(node.STATE) do
-        child ←CHILD-NODE(problem, node, action)
-        if child .STATE is not in explored or frontier then
-        frontier ←INSERT(child , frontier )
-        else if child .STATE is in frontier with higher PATH-COST then
-        replace that frontier node with child 
-        
-        
-        '''
+
+def h_zero(state, goal):
+    return 0
+
+
+def get_next(openSet, fScore):
+    lowest: float = p_infinity
+    retval = None
+    for state in openSet:
+        f = fScore[state]
+        if f < lowest:
+            lowest = f
+            retval = state
+        return retval
+
+
+def a_star(problem, hueristic):
+    openSet = set()
+    closedSet = set()
+    evaluated_states = set()
+
+    gScore = dict()
+    fScore = dict()
+
+    goal = problem.goal
+    # node ←a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
+    node = SearchNode(problem.initial_state, None, None, 0, 0)
+    openSet.add(node)
+    gScore[tuple(node.state)] = 0
+    fScore[tuple(node.state)] = hueristic(node.state, goal)
+
+    cameFrom = dict()
+
+    while len(openSet) != 0:
+        current = get_current(openSet, fScore)
+        if current is None:
+            print("Failure to find valid node in known set")
+            return None
+        if problem.goal_test(current.state):
+            return solutions(current)
+        openSet.remove(current)
+        closedSet.add(current)
+        evaluated_states.add(tuple(current.state))
+        ## generate successors
+        for action in problem.actions(current.state):
+
+            child: SearchNode = make_child_node(problem, current, action)
+            f_keys = fScore.keys()
+            g_keys = gScore.keys()
+            if tuple(child.state) not in f_keys:  # gscore and fscore are supposed to be default infinity
+                fScore[tuple(child.state)] = p_infinity
+            if tuple(child.state) not in g_keys:
+                gScore[tuple(child.state)] = p_infinity
+
+            if tuple(child.state) in evaluated_states:
+                # then this node was already evaluated so leave it alone.
+                continue
+            if child not in openSet:
+                # new node discovered
+                openSet.add(child)
+            tent_g_score = gScore[tuple(current.state)] + 1
+            if tent_g_score < gScore[tuple(child.state)]:
+                # then this is best path so record it
+                cameFrom[child] = current
+                gScore[tuple(child.state)] = tent_g_score
+                fScore[tuple(child.state)] = tent_g_score + hueristic(current.state, child.state)
+
+    return None
 
 
 if __name__ == '__main__':
     if len(argv) > 1:
         start_state = argv[1]
     else:
-        start_state = [1, 3, 'b', 4, 2, 5, 7, 8, 6]
+        start_state = [1, 2, 3, 4, 5, 6, 'b', 7, 8]
     goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 'b']
-    puzzle_solve(start_state, goal_state, a_star_search, number_misplaced_nodes)
+    puzzle_solve(start_state, goal_state, new_a_star, number_misplaced_nodes)
 
     # run greedy_best_first with num_misplaced_tiles_hueristic
